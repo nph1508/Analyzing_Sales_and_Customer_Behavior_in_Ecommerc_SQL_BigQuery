05c6c77c8466d11a57a701b8d362a66965a2ee2e
Huy Nguyen <nph15082000@gmail.com>
2025-06-25 14:09:26 +0700

    Update E-Commerce_Dataset.sql

--- E-Commerce_Dataset.sql
+++ E-Commerce_Dataset.sql
@@ -10,48 +10,48 @@ group by month
 order by month;
 
 --q2
-SELECT
+select
     trafficSource.source as source,
     sum(totals.visits) as total_visits,
-    sum(totals.Bounces) as total_no_of_bounces,
-    (sum(totals.Bounces)/sum(totals.visits))* 100.00 as bounce_rate
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`
-GROUP BY source
-ORDER BY total_visits DESC;
+    sum(totals.bounces) as total_no_of_bounces,
+    (sum(totals.bounces)/sum(totals.visits))* 100.00 as bounce_rate
+from `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`
+group by source
+order by total_visits desc;
 
 --q3
-SELECT --Lấy dữ liệu theo tháng
-    'Month' AS time_type,
-    FORMAT_DATE('%Y%m', DATE(PARSE_DATE('%Y%m%d', date))) AS time,
-    trafficSource.source AS source,
-    SUM(product.productRevenue) / 1000000 AS revenue
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201706*`,
-UNNEST(hits) AS hit,
-UNNEST(hit.product) AS product
-WHERE product.productRevenue IS NOT NULL
-GROUP BY time, source
+select --Lấy dữ liệu theo tháng
+    'Month' as time_type,
+    format_date('%Y%m', date(parse_date('%Y%m%d', date))) as time,
+    trafficSource.source as source,
+    sum(product.productRevenue) / 1000000 as revenue
+from `bigquery-public-data.google_analytics_sample.ga_sessions_201706*`,
+unnest(hits) as hit,
+unnest(hit.product) as product
+where product.productRevenue is not null
+group by time, source
 
-UNION ALL -- Gộp lại với nhau
+union all -- Gộp lại với nhau
 
-SELECT --Lấy dữ liệu theo tuần
-    'Week' AS time_type,
-    FORMAT_DATE('%Y%W', DATE(PARSE_DATE('%Y%m%d', date))) AS time,
-    trafficSource.source AS source,
-    SUM(product.productRevenue) / 1000000 AS revenue
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201706*`,
-UNNEST(hits) AS hit,
-UNNEST(hit.product) AS product
-WHERE product.productRevenue IS NOT NULL
-GROUP BY time, source
+select --Lấy dữ liệu theo tuần
+    'Week' as time_type,
+    format_date('%Y%W', date(parse_date('%Y%m%d', date))) as time,
+    trafficSource.source as source,
+    SUM(product.productRevenue) / 1000000 as revenue
+from `bigquery-public-data.google_analytics_sample.ga_sessions_201706*`,
+unnest(hits) as hit,
+unnest(hit.product) as product
+where product.productRevenue is not null
+group by time, source
 
-ORDER BY time_type, revenue desc;
+order by time_type, revenue desc;
 
 --q4
 with 
 purchaser_data as(
   select
       format_date("%Y%m",parse_date("%Y%m%d",date)) as month,
-      (sum(totals.pageviews)/count(distinct fullvisitorid)) as avg_pageviews_purchase,
+      (sum(totals.pageviews)/count(distinct fullVisitorId)) as avg_pageviews_purchase,
   from `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`
     ,unnest(hits) hits
     ,unnest(product) product
@@ -84,7 +84,7 @@ order by pd.month;
 --q5
 select
     format_date("%Y%m",parse_date("%Y%m%d",date)) as month,
-    sum(totals.transactions)/count(distinct fullvisitorid) as Avg_total_transactions_per_user
+    sum(totals.transactions)/count(distinct fullVisitorId) as Avg_total_transactions_per_user
 from `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`
     ,unnest (hits) hits,
     unnest(product) product
@@ -104,39 +104,39 @@ where product.productRevenue is not null
 group by month;
 
 --q7
-WITH Customers AS ( --xác định khách hàng đã mua sphẩm
+with Customers as ( --xác định khách hàng đã mua sphẩm
   SELECT distinct fullVisitorId
-  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`,
-  UNNEST(hits) AS hits,
-  UNNEST(hits.product) AS product
-  WHERE product.v2ProductName = "YouTube Men's Vintage Henley"
-    AND product.productRevenue IS NOT NULL                       -- Chỉ tính những người có revenue
-    AND totals.transactions >= 1                                 -- Chỉ lấy người dùng có giao dịch
+  from `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`,
+  unnest(hits) as hits,
+  unnest(hits.product) as product
+  where product.v2ProductName = "YouTube Men's Vintage Henley"
+    and product.productRevenue is not null                       -- Chỉ tính những người có revenue
+    and totals.transactions >= 1                                 -- Chỉ lấy người dùng có giao dịch
 )
 
-SELECT 
-  product.v2ProductName AS other_purchased_products, 
-  SUM(product.productQuantity) AS quantity -- 
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`,
-UNNEST(hits) AS hits,
-UNNEST(hits.product) AS product
-JOIN Customers USING (fullVisitorId)
-WHERE product.v2ProductName != "YouTube Men's Vintage Henley" --Lọc danh sách các sản phẩm khác mà những khách hàng đó đã mua
-  AND product.productRevenue IS NOT NULL                        -- Chỉ tính những người có revenue
-  AND totals.transactions >= 1                                  -- Chỉ lấy người dùng có giao dịch
-GROUP BY product.v2ProductName
-ORDER BY quantity DESC;
+select 
+  product.v2ProductName as other_purchased_products, 
+  SUM(product.productQuantity) as quantity -- 
+from `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`,
+unnest(hits) as hits,
+unnest(hits.product) as product
+join Customers using (fullVisitorId)
+where product.v2ProductName != "YouTube Men's Vintage Henley" --Lọc danh sách các sản phẩm khác mà những khách hàng đó đã mua
+  and product.productRevenue is not null                       -- Chỉ tính những người có revenue
+  and totals.transactions >= 1                                  -- Chỉ lấy người dùng có giao dịch
+group by product.v2ProductName
+order by quantity desc;
 
 --q8
 with product_data as(
 select
     format_date('%Y%m', parse_date('%Y%m%d',date)) as month,
-    count(CASE WHEN eCommerceAction.action_type = '2' THEN product.v2ProductName END) as num_product_view,
-    count(CASE WHEN eCommerceAction.action_type = '3' THEN product.v2ProductName END) as num_add_to_cart,
-    count(CASE WHEN eCommerceAction.action_type = '6' and product.productRevenue is not null THEN product.v2ProductName END) as num_purchase
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
-,UNNEST(hits) as hits
-,UNNEST (hits.product) as product
+    count(case when eCommerceAction.action_type = '2' then product.v2ProductName end) as num_product_view,
+    count(case when eCommerceAction.action_type = '3' then product.v2ProductName end) as num_add_to_cart,
+    count(case when eCommerceAction.action_type = '6' and product.productRevenue is not null then product.v2ProductName end) as num_purchase
+from `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+,unnest(hits) as hits
+,unnest (hits.product) as product
 where _table_suffix between '20170101' and '20170331'
 and eCommerceAction.action_type in ('2','3','6')
 group by month
